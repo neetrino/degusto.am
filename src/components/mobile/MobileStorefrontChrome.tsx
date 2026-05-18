cd7d8be6998fa765ae6ae75c8f2b529d21be4491
Manvel-Lambaryan <manwellllambaryaniii@gmail.com>
2026-05-18 16:22:53 +0400

    profile mobile

--- src/components/mobile/MobileStorefrontChrome.tsx
+++ src/components/mobile/MobileStorefrontChrome.tsx
@@ -4,7 +4,10 @@ import type { ReactNode } from 'react';
 import { Suspense } from 'react';
 import { usePathname } from 'next/navigation';
 import { MOBILE_STOREFRONT_CHROME_BOTTOM_INSET_CLASS } from '@/constants/mobile-figma-storefront';
-import { usesStorefrontMobileChrome } from '../../lib/uses-storefront-mobile-chrome';
+import {
+  usesStorefrontMobileChrome,
+  usesStorefrontMobileHeader,
+} from '../../lib/uses-storefront-mobile-chrome';
 import { MobileStorefrontHeader } from './MobileStorefrontHeader';
 
 type MobileStorefrontChromeProps = {
@@ -22,21 +25,32 @@ export function MobileStorefrontChrome({ children }: MobileStorefrontChromeProps
     return <>{children}</>;
   }
 
+  const showMobileHeader = usesStorefrontMobileHeader(pathname);
+  const isProfileRoute = pathname.startsWith('/profile');
+
+  const contentSurfaceClass = showMobileHeader
+    ? `relative z-10 mt-[87px] flex min-h-0 flex-1 flex-col rounded-t-[30px] bg-white px-4 pt-8 ${MOBILE_STOREFRONT_CHROME_BOTTOM_INSET_CLASS} lg:mt-0 lg:flex-none lg:rounded-none lg:bg-transparent lg:px-0 lg:pb-0 lg:pt-0`
+    : `relative z-10 flex min-h-0 flex-1 flex-col bg-white px-0 pt-0 ${MOBILE_STOREFRONT_CHROME_BOTTOM_INSET_CLASS} lg:flex-none lg:bg-transparent lg:px-0 lg:pb-0 lg:pt-0`;
+
   return (
-    <div className="flex min-h-screen w-full flex-col bg-[var(--project-color)] lg:min-h-0 lg:flex-none lg:bg-transparent">
+    <div
+      className={`flex min-h-screen w-full flex-col lg:min-h-0 lg:flex-none lg:bg-transparent ${
+        isProfileRoute ? 'bg-white lg:bg-transparent' : 'bg-[var(--project-color)]'
+      }`}
+    >
       <div className="relative flex min-h-0 flex-1 flex-col overflow-x-clip">
-        <div className="pointer-events-none absolute -left-[210px] -top-[123px] h-[434px] w-[418px] rounded-full border-[80px] border-[#3E573D] lg:hidden" />
-        <div className="pointer-events-none absolute -right-[160px] -top-[184px] h-[320px] w-[360px] rounded-full border-[70px] border-[#3E573D] lg:hidden" />
+        {showMobileHeader ? (
+          <>
+            <div className="pointer-events-none absolute -left-[210px] -top-[123px] h-[434px] w-[418px] rounded-full border-[80px] border-[#3E573D] lg:hidden" />
+            <div className="pointer-events-none absolute -right-[160px] -top-[184px] h-[320px] w-[360px] rounded-full border-[70px] border-[#3E573D] lg:hidden" />
 
-        <Suspense fallback={null}>
-          <MobileStorefrontHeader />
-        </Suspense>
+            <Suspense fallback={null}>
+              <MobileStorefrontHeader />
+            </Suspense>
+          </>
+        ) : null}
 
-        <div
-          className={`relative z-10 mt-[87px] flex min-h-0 flex-1 flex-col rounded-t-[30px] bg-white px-4 pt-8 ${MOBILE_STOREFRONT_CHROME_BOTTOM_INSET_CLASS} lg:mt-0 lg:flex-none lg:rounded-none lg:bg-transparent lg:px-0 lg:pb-0 lg:pt-0`}
-        >
-          {children}
-        </div>
+        <div className={contentSurfaceClass}>{children}</div>
       </div>
     </div>
   );
